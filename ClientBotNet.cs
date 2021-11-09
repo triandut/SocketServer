59515426fc82db818fd5a98ab1ab9054e9791e13
triandut <trian7391@gmail.com>
2021-11-09 08:59:36 +0700

    done-socket-server

---
+++ ClientBotNet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace SocketServer
+{
+    public class ClientBotNet
+    {
+        public string MacAddress;
+        public Socket socket;
+
+        public ClientBotNet(Socket socketClient, string mac)
+        {
+            socket = socketClient;
+            MacAddress = mac;
+        }
+    }
+}
